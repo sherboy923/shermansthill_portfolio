767418d038753f3b44b1d1db6d35f10b71f3f311
Sherman St. Hill <scsthill@gmail.com>
2014-01-09 16:48:34 -0600

    Finish making form auto-adjustable

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -2,14 +2,16 @@ class Inquiry
 	extend ActiveModel::Naming
 	include ActiveModel::Conversion
 	include ActiveModel::Validations
-
-	attr_accessor :name, :email, :subject, :body
-
-	validates :name, :email, :subject, :body, :presence => true
-
-	validates :email, :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
 	
-	validates :body, :length => { :minimum => 10, :maximum => 1000 }
+	attr_accessor :name, :email, :subject, :message
+
+	validates :name, :email, :subject, :message, presence: true
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :email, format: { with: VALID_EMAIL_REGEX }
+	
+	validates :message, length: { minimum: 10, maximum: 1000 }
 	
 	def initialize(attributes = {})
 		attributes.each do |name, value|
