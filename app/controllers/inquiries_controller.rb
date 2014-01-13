ee98ef7cb7fcbec8dfb79811484b72b1b13d2cbc
Sherman St. Hill <scsthill@gmail.com>
2014-01-13 06:52:50 -0600

    Finish enabling recaptcha verification

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -7,12 +7,18 @@ class InquiriesController < ApplicationController
     def create
         @inquiry = Inquiry.new(params[:inquiry])
 
-        if @inquiry.valid?
-            flash[:success] = "Your email was successfully sent!"
-            # InquiriesMailer.new_inquiry(@inquiry).deliver!
-            
-            redirect_to contact_path #, :flash => { :success => "Your email was successfully sent!" }
+        if verify_recaptcha
+            if @inquiry.valid?
+                flash[:success] = "Your email was successfully sent!"                
+                # InquiriesMailer.new_inquiry(@inquiry).deliver!
+
+                redirect_to contact_path #, :flash => { :success => "Your email was successfully sent!" }
+            else
+                render :new
+            end
         else
+            flash.delete(:recaptcha_error)
+            flash.now[:danger] = "reCAPTCHA is incorrect. Please try again."
             render :new
         end
     end
