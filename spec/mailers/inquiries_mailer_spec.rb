95d96d49d3c8732c59bdded9c0ac43d7c4199e2f
Sherman St. Hill <scsthill@gmail.com>
2014-01-18 01:58:23 -0600

    Finish adding tests to InquiryMailer spec

--- spec/mailers/inquiries_mailer_spec.rb
+++ spec/mailers/inquiries_mailer_spec.rb
@@ -1,5 +1,20 @@
 require "spec_helper"
 
 describe InquiriesMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe "new inquiry" do
+		let(:inquiry) { mock_model Inquiry, name: 'John Doe', email: 'shermansthill@gmail.com', subject: 'Hello, World!', message: 'This is my message to you.' }
+		let(:mail) { InquiriesMailer.new_inquiry(inquiry) }
+
+		it 'renders the subject' do
+			mail.subject.should == 'Hello, World!'
+		end
+
+		it 'renders the receiver email' do
+			mail.to.should == [inquiry.email]
+		end
+
+		it 'renders the sender email' do
+			mail.from.should == ['noreply@shermansthill.com']
+		end
+	end
 end
